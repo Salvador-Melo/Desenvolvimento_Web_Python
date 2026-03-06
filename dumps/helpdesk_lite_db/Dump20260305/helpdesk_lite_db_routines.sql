5779f28dbf21cadb2b80511f343dea1a7dfa8cc3
Salvador-Melo <salvador.melo@gmail.com>
2026-03-05 21:16:52 -0300

    dump correto

--- dumps/helpdesk_lite_db/Dump20260305/helpdesk_lite_db_routines.sql
+++ dumps/helpdesk_lite_db/Dump20260305/helpdesk_lite_db_routines.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ecommerce
+-- Host: 127.0.0.1    Database: helpdesk_lite_db
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.28-MariaDB
+-- Server version	8.0.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,13 +16,12 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `pedidos`
+-- Dumping events for database 'helpdesk_lite_db'
 --
 
-LOCK TABLES `pedidos` WRITE;
-/*!40000 ALTER TABLE `pedidos` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pedidos` ENABLE KEYS */;
-UNLOCK TABLES;
+--
+-- Dumping routines for database 'helpdesk_lite_db'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -33,4 +32,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-13 15:25:06
+-- Dump completed on 2026-03-05 20:37:09
--- dumps/helpdesk_lite_db/Dump20260305/helpdesk_lite_db_users.sql
+++ dumps/helpdesk_lite_db/Dump20260305/helpdesk_lite_db_users.sql
@@ -1,8 +1,8 @@
 -- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: ecommerce
+-- Host: 127.0.0.1    Database: helpdesk_lite_db
 -- ------------------------------------------------------
--- Server version	5.5.5-10.4.28-MariaDB
+-- Server version	8.0.45
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,12 +16,13 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Dumping data for table `itens_pedido`
+-- Dumping data for table `users`
 --
 
-LOCK TABLES `itens_pedido` WRITE;
-/*!40000 ALTER TABLE `itens_pedido` DISABLE KEYS */;
-/*!40000 ALTER TABLE `itens_pedido` ENABLE KEYS */;
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'Marina Alves','marina.alves@campus.edu.br','customer','2026-03-04 16:08:07'),(2,'João Henrique Lima','joao.lima@campus.edu.br','customer','2026-03-04 16:08:07'),(3,'Camila Rocha','camila.rocha@campus.edu.br','customer','2026-03-04 16:08:07'),(4,'Lucas Ferreira','lucas.ferreira@campus.edu.br','customer','2026-03-04 16:08:07'),(5,'Beatriz Nogueira','beatriz.nogueira@campus.edu.br','customer','2026-03-04 16:08:07'),(6,'Rafael Costa','rafael.costa@campus.edu.br','customer','2026-03-04 16:08:07'),(7,'Fernanda Souza','fernanda.souza@campus.edu.br','customer','2026-03-04 16:08:07'),(8,'Eduardo Martins','eduardo.martins@campus.edu.br','agent','2026-03-04 16:08:07'),(9,'Patrícia Gomes','patricia.gomes@campus.edu.br','agent','2026-03-04 16:08:07'),(10,'Carlos Mendes','carlos.mendes@campus.edu.br','agent','2026-03-04 16:08:07');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -33,4 +34,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-05-13 15:25:06
+-- Dump completed on 2026-03-05 20:37:08
